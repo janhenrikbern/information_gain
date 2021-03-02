d65e51e6ef0d95055a5db1757e5a6952b8733aaf
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2021-03-01 20:14:26 -0500

    general setup steps

---
+++ scripts/setup_base.py
@@ -0,0 +1,9 @@
+"""
+Base directory setups
+"""
+import fileinput, glob, shutil, os
+import generate_permutations
+
+os.mkdir("/content/gt")
+os.mkdir("/content/permutations")
+os.mkdir("/content/base")
