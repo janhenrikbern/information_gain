b45b2bb8c09c9545b2df97dd3b34435b3bea009b
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2021-03-01 20:13:32 -0500

    adding setup step to Deschaintre

--- scripts/setup_deschaintre.py
+++ scripts/setup_deschaintre.py
@@ -1,4 +1,8 @@
-import fileinput, glob
+"""
+Colab configurations to run flexible SVBRDF acquisition.
+"""
+
+import fileinput, glob, shutil, os
 
 v1_import_statement = 'import tensorflow as tf'
 v2_import_statement = 'import tensorflow.compat.v1 as tf \ntf.disable_v2_behavior()'
@@ -14,3 +18,7 @@ final = "max(int(multiprocessing.cpu_count() / 4), 1))"
 with fileinput.FileInput("dataReader.py", inplace=True, backup='.bak') as file:
     for line in file:
         print(line.replace(initial, final), end='')
+
+test_path = "/content/multi-image-deepNet-SVBRDF-acquisition/testImagesExamples/test"
+if not os.path.exists(test_path):
+  os.mkdir("/content/multi-image-deepNet-SVBRDF-acquisition/testImagesExamples/test")
