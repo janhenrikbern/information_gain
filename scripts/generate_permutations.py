33face9add6ec125f62a3a0f4c144dd6440d81b6
Jan Bernhard <janhenrikbern@users.noreply.github.com>
2021-03-01 20:14:11 -0500

    write out ground truth without manipulations

--- scripts/generate_permutations.py
+++ scripts/generate_permutations.py
@@ -3,11 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from image_stitcher import segment, stitch
 
-path = "test.png"
+def run(ground_truth_path, gt_out_dir="/content/gt/", permutations_out_dir="/content/permutations/"):
+    im = Image.open(ground_truth_path)
+    im = np.array(im)
+    data = segment(im)
 
-im = Image.open(path)
-im = np.array(im)
-data = segment(im)
+    stitch(data["X"], permutations_out_dir, name=None, targets=4, use_combinations=True, img_cnt=5)    
+    for k, out in enumerate(data["y"]):
+        cv2.imwrite(
+                os.path.join(gt_out_dir, f"target-{k}.png"),
+                out,
+            )
 
-stitch(data["X"], "test/", name=None, targets=4, use_combinations=True, img_cnt=5)
-stitch(data["Y"], "truth/", name=None, targets=0, use_combinations=False, img_cnt=4)
+
+if __name__ == "__main__":
+    for img in glob.glob("/content/gt/*.png")
+        run(img)
