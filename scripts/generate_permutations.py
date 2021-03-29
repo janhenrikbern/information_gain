bf3d549139545f170e48098324b95c31a2c7f878
anaikawadi <76513187+anaikawadi@users.noreply.github.com>
2021-03-29 02:04:35 -0400

    Update generate_permutations.py
    grid search view combinations

--- scripts/generate_permutations.py
+++ scripts/generate_permutations.py
@@ -5,18 +5,20 @@ import matplotlib.pyplot as plt
 from image_stitcher import segment, stitch
 import glob, os
 
-def run(ground_truth_path, gt_out_dir="/content/gt/", permutations_out_dir="/content/permutations/"):
+def run(ground_truth_path, mat, gt_out_dir="/content/gt/", permutations_out_dir="/content/permutations/test/"):
     im = cv2.imread(ground_truth_path)
     data = segment(im)
-
-    stitch(data["X"], permutations_out_dir, name=None, targets=4, use_combinations=True, img_cnt=5)    
+    stitch(data["X"], permutations_out_dir, name=mat, targets=9, use_combinations=True, img_cnt=5)    
+    os.mkdir(gt_out_dir + mat)
     for k, out in enumerate(data["y"]):
         cv2.imwrite(
-                os.path.join(gt_out_dir, f"target-{k}.png"),
+                os.path.join(gt_out_dir + mat, f"{mat}-target-{k}.png"),
                 out,
             )
 
 
 if __name__ == "__main__":
     for img in glob.glob("/content/base/*.png"):
-        run(img)
+        mat = os.path.basename(img)
+        run(img, mat=mat[:-4])
+        
